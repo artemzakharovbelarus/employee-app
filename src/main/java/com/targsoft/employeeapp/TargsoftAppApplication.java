eec79c5763b3c0fd16520ad173c88391f3ac65e1
Maria Alenskaya <malenskaya@qulix.com>
2022-03-03 15:21:35 +0300

    initial commit

---
+++ src/main/java/com/targsoft/employeeapp/TargsoftAppApplication.java
@@ -0,0 +1,13 @@
+package com.targsoft.employeeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TargsoftAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TargsoftAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/targsoft/employeeapp/TargsoftAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.targsoft.employeeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TargsoftAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
